327b5216cdc10e82eb2e015afd2948ec5a5d95af
Victor Popkov <victor@popkov.me>
2021-08-22 20:14:44 +0300

    Change code format in CMake files
    Just some first steps to consistency.

--- cmake/out_of_source_build.cmake
+++ cmake/out_of_source_build.cmake
@@ -1,12 +1,9 @@
 # Ensures that we do an out of source build
-
-MACRO(ENSURE_OUT_OF_SOURCE_BUILD DEFAULT_DIR)
-    STRING(COMPARE EQUAL "${CMAKE_SOURCE_DIR}"
-"${CMAKE_BINARY_DIR}" insource)
-    GET_FILENAME_COMPONENT(PARENTDIR ${CMAKE_SOURCE_DIR} PATH)
-    STRING(COMPARE EQUAL "${CMAKE_SOURCE_DIR}"
-"${PARENTDIR}" insourcesubdir)
-    IF(insource OR insourcesubdir)
-		set( CMAKE_BINARY_DIR "${DEFAULT_DIR}" )
-    ENDIF(insource OR insourcesubdir)
-ENDMACRO()
+macro(ENSURE_OUT_OF_SOURCE_BUILD DEFAULT_DIR)
+  string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${CMAKE_BINARY_DIR}" insource)
+  get_filename_component(PARENTDIR ${CMAKE_SOURCE_DIR} PATH)
+  string(COMPARE EQUAL "${CMAKE_SOURCE_DIR}" "${PARENTDIR}" insourcesubdir)
+  if(insource OR insourcesubdir)
+    set(CMAKE_BINARY_DIR "${DEFAULT_DIR}")
+  endif(insource OR insourcesubdir)
+endmacro()
--- cmake/thirdparty/FindLibZip.cmake
+++ cmake/thirdparty/FindLibZip.cmake
@@ -1,31 +1,30 @@
-# Locate libzip
-# This module defines
-# LIBZIP_LIBRARY
-# LIBZIP_FOUND, if false, do not try to link to libzip 
-# LIBZIP_INCLUDE_DIR, where to find the headers
+# Locate libzip This module defines LIBZIP_LIBRARY LIBZIP_FOUND, if false, do
+# not try to link to libzip LIBZIP_INCLUDE_DIR, where to find the headers
 #
 
-FIND_PATH(LIBZIP_INCLUDE_DIR zip.h
-    $ENV{LIBZIP_DIR}/include
-    $ENV{LIBZIP_DIR}
-    $ENV{OSGDIR}/include
-    $ENV{OSGDIR}
-    $ENV{OSG_ROOT}/include
-    ~/Library/Frameworks
-    /Library/Frameworks
-    /usr/local/include
-    /usr/include
-    /sw/include # Fink
-    /opt/local/include # DarwinPorts
-    /opt/csw/include # Blastwave
-    /opt/include
-    [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/include
-    /usr/freeware/include
-)
+find_path(
+  LIBZIP_INCLUDE_DIR
+  zip.h
+  $ENV{LIBZIP_DIR}/include
+  $ENV{LIBZIP_DIR}
+  $ENV{OSGDIR}/include
+  $ENV{OSGDIR}
+  $ENV{OSG_ROOT}/include
+  ~/Library/Frameworks
+  /Library/Frameworks
+  /usr/local/include
+  /usr/include
+  /sw/include # Fink
+  /opt/local/include # DarwinPorts
+  /opt/csw/include # Blastwave
+  /opt/include
+  [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/include
+  /usr/freeware/include)
 
-FIND_LIBRARY(LIBZIP_LIBRARY 
-    NAMES libzip zip
-    PATHS
+find_library(
+  LIBZIP_LIBRARY
+  NAMES libzip zip
+  PATHS
     $ENV{LIBZIP_DIR}/lib
     $ENV{LIBZIP_DIR}
     $ENV{OSGDIR}/lib
@@ -40,12 +39,9 @@ FIND_LIBRARY(LIBZIP_LIBRARY
     /opt/csw/lib
     /opt/lib
     [HKEY_LOCAL_MACHINE\\SYSTEM\\CurrentControlSet\\Control\\Session\ Manager\\Environment;OSG_ROOT]/lib
-    /usr/freeware/lib64
-)
+    /usr/freeware/lib64)
 
-SET(LIBZIP_FOUND "NO")
-IF(LIBZIP_LIBRARY AND LIBZIP_INCLUDE_DIR)
-    SET(LIBZIP_FOUND "YES")
-ENDIF(LIBZIP_LIBRARY AND LIBZIP_INCLUDE_DIR)
-
-
+set(LIBZIP_FOUND "NO")
+if(LIBZIP_LIBRARY AND LIBZIP_INCLUDE_DIR)
+  set(LIBZIP_FOUND "YES")
+endif(LIBZIP_LIBRARY AND LIBZIP_INCLUDE_DIR)
