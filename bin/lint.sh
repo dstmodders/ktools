028dde93c9886e3042f1c46c6fdcca0dd91a129a
Victor Popkov <victor@popkov.me>
2021-08-25 12:36:35 +0300

    Update lint.sh bin

--- bin/lint.sh
+++ bin/lint.sh
@@ -4,10 +4,13 @@ BASE_DIR="$(cd "$(dirname "$0")" && pwd)"
 
 cd "${BASE_DIR}/.." || exit 1
 
-cmake-lint \
-  ./**/CMakeLists.txt \
+# cmake-lint
+find . -iname 'CMakeLists.txt' -exec cmake-lint --suppress-decorations {} \;
+cmake-lint --suppress-decorations \
   ./cmake/*.cmake \
   ./cmake_uninstall.cmake.in
 
+# clang-format
 find ./src/ \( -iname '*.cpp' -o -iname '*.hpp' \) -exec \
-  clang-format -i {} \;
+  clang-format --dry-run --Werror {} \;
+clang-format --dry-run --Werror ./config.h.in
